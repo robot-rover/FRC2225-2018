e74a2c1bccf8e67350af9354a869b5f3c473d63c
robot_rover <sam.obrien00@gmail.com>
2018-01-09 23:42:36 -0600

    Initial Commit - Setting up repository

---
+++ jetson/src/main/java/frc/team2225/season2018/jetson/CoprocessorExample.java
@@ -0,0 +1,7 @@
+package frc.team2225.season2018.jetson;
+
+public class CoprocessorExample {
+    public static void main(String[] args) {
+        System.out.println("Hello World! I am a coprocessor!");
+    }
+}
---
+++ roboRIO/src/main/java/frc/team2225/season2018/roboRIO/RobotExample.java
@@ -0,0 +1,10 @@
+package frc.team2225.season2018.roboRIO;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class RobotExample extends IterativeRobot {
+    @Override
+    public void robotInit() {
+        System.out.println("Hello World! Beep Boop!");
+    }
+}
